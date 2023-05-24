4324b0560eca529fd62ac5061817931e72333935
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-24 13:53:31 -0500

    Add solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+
+    def match(self, list):
+        word_to_list = sorted([char for char in self.word])
+        match_list = []
+        for word in list:
+            word_chars_list = sorted([letter for letter in word])
+            if word_chars_list == word_to_list:
+                match_list.append(word)
+        return match_list
+
+
+
+
+
+
+  
